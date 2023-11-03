4d8314d82b63ea3ed33624cee733dbabdf1987e5
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2023-11-03 00:27:08 +0000

    think im done

--- src/app.js
+++ src/app.js
@@ -5,8 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  in the begining
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    const domains = generateDomain();
+    displayDomains(domains);
+  });
+  // console.log("Hello Rigo from the console!");
+
+  let generateDomain = () => {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    var list = []
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          list.push(pronoun[i] + adj[j] + noun[k] + ".com");
+        }
+      }
+    }return list;
+  }; let displayDomains = (domains) => {
+    const domainList = document.querySelector('#the-domain');
+    domainList.innerHTML="";
+    domains.forEach((domain) => {
+      const listItem = document.createElement("li")
+      listItem.textContent = domain;
+      domainList.appendChild(listItem);
+    })
+  }
 };
